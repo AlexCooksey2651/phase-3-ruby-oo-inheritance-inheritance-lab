4020bd5e7b9b7c4ee826904a197859ee4f132c6b
Alex Cooksey <aecooksey2651@gmail.com>
2022-02-07 15:51:34 -0500

    commit tree 1647e8ec9cf400a1034a728bc45699c240bebea0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_writer :knowledge
+    
+    def initialize
+        @knowledge = []
+    end
 
+    def knowledge
+        @knowledge
+    end
+
+    def learn (str)
+        @knowledge << str
+    end
 end
